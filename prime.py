e68be25e5e6e7027976c8d0e1e973d20160fa727
btna19 <betteenathankachan9@gmail.com>
2024-11-21 10:44:23 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,30 @@
+# num = 6
+# prime=True
+
+# if num ==1:
+#     print('one is not prime')
+# else:
+#     i=2
+#     while num>i:
+#         if num%i==0:
+#             prime=False
+#         i=i+1
+# if prime==False:
+#     print('Not prime number')
+# else:
+#     print('Prime number')
+
+# z=983257
+# digi_sum=0
+# while z!=-0:
+#     x=z%10
+#     print(z)
+#     z=z//10
+# print(digi_sum)
+
+# z=8086084928
+
+# while z!=-0:
+#     x=z%10
+#     print(z)
+#     z=z//10
